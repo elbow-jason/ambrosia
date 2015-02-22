73994d4b76a539c639d8cd2e1a43827098acf621
Jason Goldberger <jlgoldb2@asu.edu>
2015-02-22 16:53:32 -0700

    updated ambrosia to use ambrosia functions

--- lib/ambrosia.ex
+++ lib/ambrosia.ex
@@ -1,10 +1,10 @@
 defmodule Ambrosia do
 
   def init do
-    schema = :mnesia.create_schema [Node.self()]
+    schema = Ambrosia.Schema.create
     started = :mnesia.start()
     case schema do
-      {:ok, _, _} -> IO.puts "Schema accepted for #{Node.self()}"
+      :ok -> IO.puts "Schema successfully for #{Node.self()}."
       {:error, { _, {:already_exists, x}}} -> IO.puts "Schema for #{x} already exists."
       x -> IO.puts "Error creating schema: #{inspect x}"
     end
