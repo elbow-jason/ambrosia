3ba8f7c55e2aa43290f71a0f3be344f572f29910
Jason Goldberger <jlgoldb2@asu.edu>
2015-02-22 15:12:57 -0700

    ambrosia is starting and creating schema for Mnesia.nonode@nohost (Node.self) properly

--- lib/ambrosia.ex
+++ lib/ambrosia.ex
@@ -1,2 +1,19 @@
 defmodule Ambrosia do
+
+  def init do
+    schema = :mnesia.create_schema [Node.self()]
+    started = :mnesia.start()
+    case schema do
+      {:ok, _, _} -> IO.puts "Schema accepted for #{Node.self()}"
+      {:error, { _, {:already_exists, x}}} -> IO.puts "Schema for #{x} already exists."
+      x -> IO.puts "Error creating schema: #{inspect x}"
+    end
+    case started do
+      :ok -> IO.puts "Ambrosia started..."
+      x -> IO.puts "Ambrosia failed to start: #{inspect x}"
+    end
+  end
+
 end
+
+Ambrosia.init
